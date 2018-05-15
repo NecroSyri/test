0c81254add781d895b55fd24fc961e5755981e70
Crelier <jean-claude.crelier@amersports.com>
2018-05-15 15:39:26 +0200

    save lua

---
+++ buttons.lua
@@ -0,0 +1,22 @@
+button={}
+button.click_function = "none"
+button.function_owner = self
+button.label = "Button"
+button.position = {0,0,0}
+button.rotation = {0,0,0}
+button.width = 3000
+button.height = 1500
+button.font_size = 1000
+
+input={}
+
+//lvl <l>
+//att <a>
+//def <d>
+self.createButton(button)
+
+//name inp
+//xp inp
+//xpnext inp
+//hp inp
+//hpmax inp
