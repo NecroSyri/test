70bcb2915a0750c52221ef4f207b007a2877061b
Crelier <jean-claude.crelier@amersports.com>
2018-05-16 09:27:08 +0200

    idea for buttons

--- buttons.lua
+++ buttons.lua
@@ -1,3 +1,12 @@
+//tab={}
+//table.insert(tab,{"titi",21})
+//table.insert(tab,{"toto",22})
+//table.insert(tab,{"tata",23})
+//table.insert(tab,{"tutu",24})
+//print(tab[2][1].." - "..tab[2][2])
+//table.remove(tab,2)
+//print(tab[2][1].." - "..tab[2][2])
+
 button={}
 button.click_function = "none"
 button.function_owner = self
@@ -9,12 +18,13 @@ button.height = 1500
 button.font_size = 1000
 
 input={}
+buttons={}
 
 //lvl <l>
 //att <a>
 //def <d>
 self.createButton(button)
-
+//-----table.insert(buttons,{"name",self.getButtons()[self.getButtons().length].id})
 //name inp
 //xp inp
 //xpnext inp
